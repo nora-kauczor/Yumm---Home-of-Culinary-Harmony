9131fb42739b11eafd45220b3c95ddb98ea9817f
Nora Kauczor <n.kauczor@web.de>
2024-07-04 14:00:26 +0200

    use children prop in Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,10 @@
 import { NavBar } from "./NavBar";
 
-export function Layout() {
-  return <NavBar />;
+export function Layout({ children }) {
+  return (
+    <>
+      <main>{children}</main>
+      <NavBar />
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
+import styled from "styled-components";
+
+const Box = styled.div`
+  height: 3000px;
+  width: 3000px;
+  background-color: green;
+`;
+
 export default function HomePage() {
   return (
     <div>
       <h1>Hello from Next.js</h1>
+      <Box />
     </div>
   );
 }
