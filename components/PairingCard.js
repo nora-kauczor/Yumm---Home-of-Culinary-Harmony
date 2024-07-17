1df02d0b695c91fd489211c435ffcbeccc298947
Nora Kauczor <n.kauczor@web.de>
2024-07-17 21:58:43 +0200

    display pairingson an overview page, style them roughly

---
+++ components/PairingCard.js
@@ -0,0 +1,79 @@
+import { getFlavorColor } from "@/utils/getFlavorColor";
+import styled from "styled-components";
+
+const CardWrapper = styled.ul`
+  width: 100%;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
+  background-color: beige;
+  border-radius: 1rem;
+  padding: 20px;
+`;
+
+const IngredientsSection = styled.ul`
+  display: flex;
+  list-style: none;
+  gap: 20px;
+`;
+
+const IngredientName = styled.li`
+  font-size: 1.25rem;
+  padding: 0;
+  margin: 0;
+`;
+
+const FlavorsSection = styled.div`
+  display: flex;
+  gap: 15px;
+`;
+
+const FlavorTag = styled.a`
+  border-radius: 1rem;
+  padding: 10px;
+`;
+
+const ReasonSection = styled.div`
+  display: flex;
+`;
+
+export default function PairingCard({ pairing, ingredients }) {
+  if (!ingredients || !pairing) return <>Loading...</>;
+
+  function findIngredient(id) {
+    const foundIngredient = ingredients.find(
+      (ingredient) => ingredient._id === id
+    );
+    return foundIngredient;
+  }
+
+  const specificIngredients = pairing.ingredients.map((id) => {
+    return findIngredient(id);
+  });
+
+  const color0 = getFlavorColor(specificIngredients[0].flavorProfile);
+  const color1 = getFlavorColor(specificIngredients[1].flavorProfile);
+
+  return (
+    <CardWrapper>
+      <IngredientsSection>
+        <IngredientName>{specificIngredients[0].name} </IngredientName>
+        <line>🧡</line>
+        <IngredientName>{specificIngredients[1].name}</IngredientName>
+      </IngredientsSection>
+      <FlavorsSection>
+        <FlavorTag style={{ backgroundColor: color0 }}>
+          {specificIngredients[0].flavorProfile}{" "}
+        </FlavorTag>
+        <FlavorTag style={{ backgroundColor: color1 }}>
+          {specificIngredients[1].flavorProfile}{" "}
+        </FlavorTag>
+      </FlavorsSection>
+      <ReasonSection>{pairing.reason}</ReasonSection>
+    </CardWrapper>
+  );
+}
