04f6cac759b1c7f617661655de62e7ab2254cd23
Nora Kauczor <n.kauczor@web.de>
2024-07-22 20:03:10 +0200

    style homepage roughly

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,28 @@
 import styled from "styled-components";
 
+const HomePageContent = styled.a`
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+`;
+
 export default function HomePage() {
   return (
-    <div>
-      <a>
-        <h3>It&apos;s nice to have you here.</h3>
-        <p>
-          {" "}
-          Yumm! is your ultimate guide to the world of flavors. Whether
-          you&apos;re a professional chef or a home cook, Yumm! helps you
-          discover, store, and master the art of flavor combinations. Our app
-          provides a vast library of flavors and ingredients, along with expert
-          advice on how to combine them to create mouth-watering dishes.{" "}
-        </p>{" "}
-        <p>
-          Whether you&apos;re experimenting in the kitchen or looking to perfect
-          a classic dish, Yumm! is your go-to app for all things flavor. Unlock
-          your culinary potential with Yumm! and make every meal a delicious
-          adventure.
-        </p>
-      </a>
-    </div>
+    <HomePageContent>
+      <h3>It&apos;s nice to have you here.</h3>
+      <p>
+        {" "}
+        Yumm! is your ultimate guide to the world of flavors. Whether
+        you&apos;re a professional chef or a home cook, Yumm! helps you
+        discover, store, and master the art of flavor combinations. Our app
+        provides a vast library of flavors and ingredients, along with expert
+        advice on how to combine them to create mouth-watering dishes.{" "}
+      </p>{" "}
+      <p>
+        Whether you&apos;re experimenting in the kitchen or looking to perfect a
+        classic dish, Yumm! is your go-to app for all things flavor. Unlock your
+        culinary potential with Yumm! and make every meal a delicious adventure.
+      </p>
+    </HomePageContent>
   );
 }
